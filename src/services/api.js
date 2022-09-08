38502c065500d8f7c10963eecd77654fc21f613b
João Matheus Silva Franca <matheussfranca@live.com>
2022-09-08 15:25:20 -0300

    Concluído: Requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,30 @@
 export async function getCategories() {
-  // Implemente aqui
+  const ENDPOINT = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(ENDPOINT);
+  const data = await response.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const ENDPOINT_01 = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const ENDPOINT_02 = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  if (categoryId !== undefined) {
+    const response = await fetch(ENDPOINT_01);
+    const data = await response.json();
+    return data;
+  } 
+  if (query !== undefined) {
+    const response = await fetch(ENDPOINT_02);
+    const data = await response.json();
+    return data;
+  }
+  else {
+    
+  }
+
 }
 
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
 }
-
-// Grupo 33
