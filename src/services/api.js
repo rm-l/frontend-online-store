42f6720d89c32a6bc3792e4182e6883e6fa63b06
João Matheus Silva Franca <matheussfranca@live.com>
2022-09-08 15:12:33 -0300

    Iniciando requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -10,3 +10,5 @@ export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
 }
+
+// Grupo 33
